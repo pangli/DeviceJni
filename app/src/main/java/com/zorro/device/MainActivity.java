1cf6c7d4fd37c4903018816ddc64d909019c5ed9
Zorro <zorro@cashrocket.com.ph>
2024-04-16 14:47:16 +0800

    init c++

--- app/src/main/java/com/zorro/device/MainActivity.java
+++ app/src/main/java/com/zorro/device/MainActivity.java
@@ -22,6 +22,6 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         TextView textView = findViewById(R.id.textView);
-        textView.setText("jfoiueoiuofweuo");
+        textView.setText(new NativeLib().stringFromJNI());
     }
 }
